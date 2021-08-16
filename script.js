d09823141f3943dde3e928c60db324798eb2f8e5
Shetest <ellen.santtis@gmail.com>
2021-08-15 21:11:22 -0300

    Iniciando o js

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    console.log('iniciando');
+    eventClickButton();
+};
+
+const eventClickButton = () => {
+    const clickButon = document.querySelector('#search-button');
+    clickButon.addEventListener('click', eventGetText )
+};
+
+const eventGetText = () => {
+    const getText = document.querySelector('#currency-input').value;
+    console.log(getText);
+};
