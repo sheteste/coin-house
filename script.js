09dd90b41cd1508d0e8ac9bc7a8c6b194a75c47d
Shetest <ellen.santtis@gmail.com>
2021-08-16 13:08:46 -0300

    atualizando o html e js

--- script.js
+++ script.js
@@ -5,24 +5,45 @@ window.onload = () => {
 
 const eventClickButton = () => {
     const clickButon = document.querySelector('#search-button');
-    clickButon.addEventListener('click', eventGetText )
+    clickButon.addEventListener('click', eventGetText)
 };
 
 const eventGetText = () => {
     const getText = document.querySelector('#currency-input').value;
-    console.log(getText);
+    clearList();
+    fetchCurrency(getText);
 };
 
-const fetchCurrency = () => {
-    const saveFetch = fetch('https://api.exchangerate.host/latest')
-    .then((response) => response.json())
-    .then((object) => {
-        const rates = object.rates;
-        const ratesEntries = Object.entries(rates)
-        console.log(ratesEntries);
-    })
-    .catch((error) => console.log(error, "eroooooou"))
-    
+const fetchCurrency = (currency) => {
+    const saveFetch = fetch(`https://api.exchangerate.host/latest?base=${currency}`)
+        .then((response) => response.json())
+        .then((object) => {
+            baseCurrency(object.base);
+            const rates = object.rates;
+            const ratesEntries = Object.entries(rates)
+
+            const currencyList = document.querySelector('#currency-list');
+
+            ratesEntries.forEach((entry) => {
+                const currency = entry[0];
+                const value = entry[1]
+
+
+                const itemCurrencyList = document.createElement('li');
+                itemCurrencyList.innerText = `${currency}: ${value}`
+                currencyList.appendChild(itemCurrencyList);
+                console.log(itemCurrencyList);
+            })
+        })
+        .catch((error) => console.log(error, "eroooooou"))
 }
 
-fetchCurrency();
+const clearList = () => {
+    const currencyList = document.querySelector('#currency-list');
+    currencyList.innerHTML = ' ';
+}
+
+const baseCurrency = (base) => {
+    const baseTitle = document.querySelector('#base');
+    baseTitle.innerHTML = `Valores referenes a 1 ${base}`;
+}
